c63fd17e72472395c5f27f13b97d6f1526413967
Carlos20040301 <80127423+Carlos20040301@users.noreply.github.com>
2022-11-17 17:57:28 -0600

    TOT

---
+++ Flashbook/src/flashbook/Flashbook.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package flashbook;
+
+/**
+ *
+ * @author edcastro
+ * 
+ */
+
+
+public class Flashbook {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        new Inicio().setVisible(true);
+        
+        int edad;
+        String nombre,correo,contra; 
+        // TODO code application logic here
+    }
+    
+}
